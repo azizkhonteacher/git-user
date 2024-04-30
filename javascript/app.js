2177a61d47e2662bfa56197343cb1406b763170f
azizkhonteacher <azizkhonteacher@gmail.com>
2024-04-30 20:17:45 +0500

    sa

--- javascript/app.js
+++ javascript/app.js
@@ -2,5 +2,5 @@ const html = document.querySelector("html");
 const a = document.querySelector(".site__nav-btn");
 
 a.addEventListener("click", () => {
-  html.classList.toggle(".dark");
+  html.classList.toggle("dark");
 });
