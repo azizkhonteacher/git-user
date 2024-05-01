118e02bfc1e115f6520f5351ac4043569f5a2fac
azizkhonteacher <azizkhonteacher@gmail.com>
2024-05-01 09:08:42 +0500

    dark mode tugadi

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,25 @@
 const html = document.querySelector("html");
-const a = document.querySelector(".site__nav-btn");
+const changeMode = document.querySelector(".site__nav-btn");
+const changeModeP = document.querySelector(".site__nav-btn p");
 
-a.addEventListener("click", () => {
-  html.classList.toggle("dark");
+let dark = JSON.parse(localStorage.getItem("mode")) ? true : false;
+
+if (dark) {
+  html.classList.add("dark");
+  changeModeP.textContent = "light";
+} else {
+  html.classList.remove("dark");
+  changeModeP.textContent = "dark";
+}
+
+changeMode.addEventListener("click", () => {
+  if (JSON.parse(localStorage.getItem("mode"))) {
+    html.classList.remove("dark");
+    localStorage.setItem("mode", false);
+    changeModeP.textContent = "dark";
+  } else {
+    html.classList.add("dark");
+    localStorage.setItem("mode", true);
+    changeModeP.textContent = "light";
+  }
 });
